1170c60de2fa56046497bb6e5280ce4ae01823ad
David N <paperish@icloud.com>
2025-10-25 00:14:12 -0600

    Finished task list lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form'); //add event listener at form
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); //prevent from submitting
+
+    const taskDescription = document.getElementById('new-task-description').value; //call task description
+
+    buildToDo(taskDescription); //pass value to function
+
+  });
 });
+
+function buildToDo(taskItem) {
+  const li = document.createElement('li'); //add submitted input to list item
+  li.textContent = taskItem;
+
+  const task = document.getElementById('tasks'); //append list item to ul
+  task.appendChild(li);
+};
